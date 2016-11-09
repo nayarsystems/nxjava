0f0a3ea9f7b28b52f7ce95e75715f27888f23cd5
Daniel Pecos Martinez <contact@danielpecos.com>
2016-11-09 16:09:28 +0100

    Examples refactored to get rid off threads

--- src/main/java/com/nayarsystems/nexus/demo/PipesExample.java
+++ src/main/java/com/nayarsystems/nexus/demo/PipesExample.java
@@ -13,54 +13,43 @@ public class PipesExample {
     public static void main(String[] args) throws URISyntaxException {
         URI uri = new URI("ws://localhost");
 
-        new Thread(() -> {
-            NexusClient client = new NexusClient(uri);
+        NexusClient client = new NexusClient(uri);
 
-            client.login("root", "root", (x) -> {
+        client.login("root", "root", (x) -> {
 
-                client.pullTask("demo.pipes", null, (Task task) -> {
-                    String pipeId = (String) task.getParameters().get("pipe");
+            client.pullTask("demo.pipes", null, (Task task) -> {
+                String pipeId = (String) task.getParameters().get("pipe");
 
-                    Pipe pipe = client.pipeOpen(pipeId);
+                Pipe pipe = client.pipeOpen(pipeId);
 
-                    pipe.write("Processing...", (res) -> {
+                pipe.write("Processing...", (res) -> {
 
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-                        task.sendResult(ImmutableMap.of("res","ok"), (r) -> client.close());
-                    });
-                });
-
-            });
-
-        }).start();
-
-
-        new Thread(() -> {
-            NexusClient client = new NexusClient(uri);
-
-            client.login("root", "root", (x) -> {
-
-                client.pipeCreate(1, (pipe) -> {
-                    System.out.println("Pipe created with ID " + pipe.getId());
-
-                    pipe.read(1, 1, (data) -> {
-                        System.out.println(data);
-                    });
-
-                    client.pushTask("demo.pipes.test", ImmutableMap.of("pipe", pipe.getId()), null, null, null, null, (response) -> {
-                        System.out.println(response);
-
-                        pipe.close((r) -> client.close());
-                    });
-
+                    task.sendResult(ImmutableMap.of("res","ok"), (r) -> client.close());
                 });
             });
 
-        }).start();
+
+            client.pipeCreate(1, (pipe) -> {
+                System.out.println("Pipe created with ID " + pipe.getId());
+
+                pipe.read(1, 1, (data) -> {
+                    System.out.println(data);
+                });
+
+                client.pushTask("demo.pipes.test", ImmutableMap.of("pipe", pipe.getId()), null, null, null, null, (response) -> {
+                    System.out.println(response);
+
+                    pipe.close((r) -> client.close());
+                });
+
+            });
+        });
+
     }
 }
